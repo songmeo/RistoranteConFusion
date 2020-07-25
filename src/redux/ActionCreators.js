1533fd4d93c09da59f776147b0aa6da4f1797e79
songmeo <shuong1012@gmail.com>
2020-07-25 10:11:38 +0300

    fetch from server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -49,3 +49,25 @@ export const addComments = (comments) => ({
     type: ActionTypes.ADD_COMMENTS,
     payload: comments
 });
+
+export const fetchPromos = () => (dispatch) =>  {
+    dispatch(promosLoading(true));
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)));
+}
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+})
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
+});
