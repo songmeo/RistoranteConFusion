4f449d249327b6a3c978242c13468017bb6add52
songmeo <shuong1012@gmail.com>
2020-07-26 12:35:27 +0300

    animations

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,6 +5,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';	
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -85,13 +86,18 @@ class CommentForm extends React.Component {
 
 function RenderDish({ dish }) {
 	return(
-		<Card>
-			<CardImg top src={baseUrl + dish.image} alt={dish.name} />
-			<CardBody>
-				<CardTitle>{dish.name}</CardTitle>
-				<CardText>{dish.description}</CardText>
-			</CardBody>
-		</Card>
+		<FadeTransform in
+			transformProps={{
+				exitTransform: 'scale(0.5) translateY(-50%)'
+			}}>
+			<Card>
+				<CardImg top src={baseUrl + dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>
+			</Card>
+		</FadeTransform>
 	);
 }
 
@@ -100,8 +106,12 @@ function RenderComments({ comments, postComment, dishID }) {
 		comments = comments.map((comment) => {
 			return (
 				<ul key={comment.id} className = "list-unstyled">
-					<li>{ comment.comment }</li>
-					<li>-- { comment.author }, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+					<Stagger in>
+						<Fade in>
+							<li>{ comment.comment }</li>
+							<li>-- { comment.author }, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+						</Fade>
+					</Stagger>
 				</ul>
 			);
 		});
